29fee1deab23b8300c6b795186f33450b9746dfc
muggle <isocket@outlook.com>
2020-04-18 09:58:22 +0800

    webflux 版本支持

--- src/main/java/com/muggle/poseidon/handler/RoolMessageHandler.java
+++ src/main/java/com/muggle/poseidon/handler/RoolMessageHandler.java
@@ -1,14 +1,17 @@
+/*
 package com.muggle.poseidon.handler;
 
 import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
 
+*/
 /**
  * @program: poseidon-cloud-starter
  * @description: 权限信息处理器
  * 废弃原因——加入到开机任务当中
  * @author: muggle
  * @create: 2019-11-05
- **/
+ **//*
+
 @Deprecated
 public class RoolMessageHandler {
 
@@ -16,3 +19,4 @@ public class RoolMessageHandler {
 
 
 }
+*/
